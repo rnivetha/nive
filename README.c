9b783248a14124168022aa531ce3fe11f9455529
rnivetha <rnivetha1995@gmail.com>
2016-07-08 22:31:47 -0700

    finish

---
+++ README.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the num",n);
+scanf(%d,&n);
+if(n>o)
+{
+printf("num is positive");
+else
+printf("num is negative");
+}
+else
+printf("num is zero");
+}
